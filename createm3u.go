a2a85ff006bab883ef5ded9a68d4764f4443b3fd
Brandon Harper <brandon.harper@twcable.com>
2014-04-25 10:20:45 -0600

    Now detects Mp3 files

--- createm3u.go
+++ createm3u.go
@@ -2,6 +2,7 @@ package main
 
 import "io/ioutil"
 import "log"
+import "strings"
 
 func main() {
 
@@ -12,8 +13,16 @@ func main() {
 	}
 
 	for _, entry := range entries {
-		log.Println(entry.Name())
+		if isMusicFile(entry.Name()) {
+			log.Println(entry.Name() + " is a music file!")
+		} else {
+			log.Println(entry.Name() + " is not a music file.")
+		}
 	}
 
 	log.Println("No more files found!")
+}
+
+func isMusicFile(s string) bool {
+	return strings.HasSuffix(s, ".mp3")
 }
